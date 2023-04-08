40b1582e80c03e2f55565da84950493abd30f78f
Ali Firoozabadi <105077900+alifiroozabadi@users.noreply.github.com>
2023-04-08 12:09:08 +0330

    Delete Create-VirtualMachine directory

--- Create-VirtualMachine/CreateMultipleVMs.tf
+++
@@ -1,9 +0,0 @@
-module "Create-VM1" {
-  source = "./module/Create-VM"
-  vsphere_name  = "vm1"
-}
-
-module "Create-VM2" {
-  source = "./module/Create-VM"
-  vsphere_name  = "Vvm2"
-}
--- Create-VirtualMachine/CreateVM.tf
+++
@@ -1,3 +0,0 @@
-module "Create-VM" {
-  source = "./module/Create-VM"
-}
